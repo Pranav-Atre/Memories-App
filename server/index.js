b8db12c777084a4ad30a8d0844c5e438d0f0c098
HarshalAtre <113234343+HarshalAtre@users.noreply.github.com>
2024-08-21 13:42:55 +0530

    Updated

--- server/index.js
+++ server/index.js
@@ -9,7 +9,12 @@ import dotenv from 'dotenv';
 const app = express();
 dotenv.config();
 
-
+app.get(
+    "/",
+    (req, res) => {
+        res.send("Hello World");
+        }
+)
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
